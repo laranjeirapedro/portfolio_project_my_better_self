f89b006b38b024082d4b25953b5cf1fa4b652686
Rodrigo Garbulha <rodrigo.garbulha@gmail.com>
2024-08-22 15:13:50 -0400

    move adsense script

--- apps/blog-fe/pages/_app.tsx
+++ apps/blog-fe/pages/_app.tsx
@@ -33,11 +33,6 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8361311161311634"
-          crossOrigin="anonymous"
-        ></script>
         <meta
           name="google-adsense-account"
           content="ca-pub-8361311161311634"
--- apps/blog-fe/pages/_document.tsx
+++ apps/blog-fe/pages/_document.tsx
@@ -1,15 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8361311161311634"
           crossOrigin="anonymous"
-        />
+        ></script>
       </Head>
       <body>
         <Main />
