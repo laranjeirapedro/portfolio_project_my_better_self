87f5ee7f336bdf1731a84a76b9d8b1eb76f00bb1
Rodrigo Garbulha <46817338+RGarbulha@users.noreply.github.com>
2024-11-10 13:57:12 -0500

    Fix/fix published visibility (#48)
    * chore: initital setup arcanehut marketing site
    
    * fix: fix cms blog post publish status visibility

--- packages/hooks/src/context/sanityClient/sanityClientProvider.tsx
+++ packages/hooks/src/context/sanityClient/sanityClientProvider.tsx
@@ -1,4 +1,4 @@
-import { createClient } from "@sanity/client";
+import { ClientPerspective, createClient } from "@sanity/client";
 import { createContext, useContext } from "react";
 
 type SanityClientData = { client: any };
@@ -7,12 +7,17 @@ type SanityClientProviderProps = {
   children: React.ReactNode;
 };
 
+const CMSPerspective: ClientPerspective =
+  (process.env.NEXT_PUBLIC_SANITY_PERSPECTIVE as ClientPerspective) ??
+  "published";
+
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   useCdn: true, // set to `false` to bypass the edge cache
   apiVersion: "2022-03-07", // use current date (YYYY-MM-DD) to target the latest API version
   token: process.env.NEXT_PUBLIC_SANITY_ACCESS_TOKEN,
+  perspective: CMSPerspective,
 });
 
 const SanityClientContext = createContext<SanityClientData>({ client });
